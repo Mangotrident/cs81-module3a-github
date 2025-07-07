a9b25fbf34e024acc4790cf59cc1e3370c1e2f5c
Mangotrident <88416293+Mangotrident@users.noreply.github.com>
2025-07-06 19:30:57 -0700

    journal.js

--- journal.js
+++ journal.js
@@ -1,1 +1,30 @@
 
+// Greet a user by name
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+//add two numbers
+function add(a,b) {
+  return a+b;
+}
+console.log(`2+3 =`, add(2,3));
+
+//Check if a number is even
+function iseven(num) {
+  return num % 2 == 0;
+}
+console.log(`is 6 even?`, iseven(4));
+
+//Check if a number is odd
+function isodd(odd) {
+  return num % 2 != 0;
+}
+console.log(`is 5 odd?`, isodd(4));
+
+//Find max in an arrAY
+function findmax(arr) {
+  return Math.max(...arr);
+}
+console.log(`Max of [1,4,5,8,6] is`, findmax([1,4,5,8,6]));
+
